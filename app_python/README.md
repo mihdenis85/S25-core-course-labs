f720d0f235d42cb2d76f5fbe7d8e9aa800602b6e
mihdenis85 <mihdenis777@yandex.ru>
2025-01-27 00:01:21 +0300

    Lab 2 Denis Mikhailov SD-03

--- app_python/README.md
+++ app_python/README.md
@@ -21,9 +21,27 @@ source .venv/bin/activate
 pip install -r requirements.txt
 ```
 
-- Run the application and test it:
+- Run the application (via `gunicorn`) and test:
 
 ```bash
-python app.py
-curl localhost:5000
+python -m gunicorn --bind 0.0.0.0:8080 app:app
+curl localhost:8080
+```
+
+## Docker
+
+### Build
+
+```bash
+cd S25-core-course-labs/app_python
+docker build --tag docker_username/app_python:v1.0 .
+```
+
+Instead of docker username, use your own one.
+
+### Pull and Run
+
+```bash
+docker pull mihdenis85/app_python:v1.0
+docker run -p 8080:8080 mihdenis85/app_python:v1.0
 ```
--- app_python/app.py
+++ app_python/app.py
@@ -1,20 +1,29 @@
 from datetime import datetime
+from os import environ
 
 import pytz
 from flask import Flask, render_template
 
-app = Flask(__name__)
+HOST = environ.get("HOST", "0.0.0.0")
+PORT = int(environ.get("PORT", "8080"))
 
 
-@app.route("/", methods=["GET"])
-def index():
-    """
-    Returns html page with current Moscow time.
-    """
-    moscow_timezone = pytz.timezone('Europe/Moscow')
-    time = datetime.now(moscow_timezone)
-    return render_template("index.html", time=time)
+def create_app():
+    app = Flask(__name__)
 
+    @app.route("/", methods=["GET"])
+    def index():
+        """
+        Returns html page with current Moscow time.
+        """
+        moscow_timezone = pytz.timezone('Europe/Moscow')
+        time = datetime.now(moscow_timezone)
+        return render_template("index.html", time=time)
+
+    return app
+
+
+app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host=HOST, port=PORT, debug=False)

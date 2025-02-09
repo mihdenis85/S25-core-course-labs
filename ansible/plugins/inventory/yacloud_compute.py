bed0c5ae5c424c9be3aea27b83e7746a9853d443
mihdenis85 <mihdenis777@yandex.ru>
2025-02-09 23:21:06 +0300

    Lab 5 Denis Mikhailov SD-03

--- ansible/plugins/inventory/yacloud_compute.py
+++ ansible/plugins/inventory/yacloud_compute.py
@@ -4,7 +4,7 @@ from __future__ import (absolute_import, division, print_function)
 
 __metaclass__ = type
 
-DOCUMENTATION = '''
+DOCUMENTATION = """
     name: yacloud_compute
     plugin_type: inventory
     short_description: Yandex.Cloud compute inventory source
@@ -38,10 +38,10 @@ DOCUMENTATION = '''
             type: string
             default: ""
 
-'''
+"""
 
-EXAMPLES = '''
-'''
+EXAMPLES = """
+"""
 
 from ansible.errors import AnsibleError
 from ansible.plugins.inventory import BaseInventoryPlugin, Constructable, Cacheable
@@ -56,22 +56,25 @@ try:
     from yandex.cloud.resourcemanager.v1.cloud_service_pb2 import ListCloudsRequest
     from yandex.cloud.resourcemanager.v1.cloud_service_pb2_grpc import CloudServiceStub
     from yandex.cloud.resourcemanager.v1.folder_service_pb2 import ListFoldersRequest
-    from yandex.cloud.resourcemanager.v1.folder_service_pb2_grpc import FolderServiceStub
+    from yandex.cloud.resourcemanager.v1.folder_service_pb2_grpc import (
+        FolderServiceStub,
+    )
 except ImportError:
-    raise AnsibleError('The yacloud dynamic inventory plugin requires yandexcloud')
+    raise AnsibleError("The yacloud dynamic inventory plugin requires yandexcloud")
 
 display = Display()
 
 
 class InventoryModule(BaseInventoryPlugin, Constructable, Cacheable):
-    NAME = 'yacloud_compute'
+    NAME = "yacloud_compute"
 
     def verify_file(self, path):
         if super(InventoryModule, self).verify_file(path):
-            if path.endswith(('yacloud_compute.yml', 'yacloud_compute.yaml')):
+            if path.endswith(("yacloud_compute.yml", "yacloud_compute.yaml")):
                 return True
         display.debug(
-            "yacloud_compute inventory filename must end with 'yacloud_compute.yml' or 'yacloud_compute.yaml'")
+            "yacloud_compute inventory filename must end with 'yacloud_compute.yml' or 'yacloud_compute.yaml'"
+        )
         return False
 
     def _get_ip_for_instance(self, instance):
@@ -87,31 +90,39 @@ class InventoryModule(BaseInventoryPlugin, Constructable, Cacheable):
 
     def _get_clouds(self):
         all_clouds = MessageToDict(self.cloud_service.List(ListCloudsRequest()))["clouds"]
-        if self.get_option('yacloud_clouds'):
-            all_clouds[:] = [x for x in all_clouds if x["name"] in self.get_option('yacloud_clouds')]
+        if self.get_option("yacloud_clouds"):
+            all_clouds[:] = [x for x in all_clouds if x["name"] in self.get_option("yacloud_clouds")]
         self.clouds = all_clouds
 
     def _get_folders(self):
         all_folders = []
         for cloud in self.clouds:
-            all_folders += MessageToDict(self.folder_service.List(ListFoldersRequest(cloud_id=cloud["id"])))["folders"]
+            all_folders += MessageToDict(
+                self.folder_service.List(ListFoldersRequest(cloud_id=cloud["id"]))
+            )["folders"]
 
-        if self.get_option('yacloud_folders'):
-            all_folders[:] = [x for x in all_folders if x["name"] in self.get_option('yacloud_folders')]
+        if self.get_option("yacloud_folders"):
+            all_folders[:] = [
+                x
+                for x in all_folders
+                if x["name"] in self.get_option("yacloud_folders")
+            ]
 
         self.folders = all_folders
 
     def _get_all_hosts(self):
         self.hosts = []
         for folder in self.folders:
-            hosts = self.instance_service.List(ListInstancesRequest(folder_id=folder["id"]))
+            hosts = self.instance_service.List(
+                ListInstancesRequest(folder_id=folder["id"])
+            )
             dict_ = MessageToDict(hosts)
 
             if dict_:
                 self.hosts += dict_["instances"]
 
     def _init_client(self):
-        file = self.get_option('yacloud_token_file')
+        file = self.get_option("yacloud_token_file")
         token_var = self._vars.get("yacloud_token")
 
         if file is not None:
@@ -119,11 +130,12 @@ class InventoryModule(BaseInventoryPlugin, Constructable, Cacheable):
         elif token_var is not None:
             token = token_var
         else:
-            token = self.get_option('yacloud_token')
+            token = self.get_option("yacloud_token")
         if not token:
             raise AnsibleError(
                 "token it empty. provide either "
-                "`yacloud_token_file` or `yacloud_token`")
+                "`yacloud_token_file` or `yacloud_token`"
+            )
         sdk = yandexcloud.SDK(token=token)
 
         self.instance_service = sdk.client(InstanceServiceStub)
@@ -131,7 +143,7 @@ class InventoryModule(BaseInventoryPlugin, Constructable, Cacheable):
         self.cloud_service = sdk.client(CloudServiceStub)
 
     def _process_hosts(self):
-        group_label = str(self.get_option('yacloud_group_label'))
+        group_label = str(self.get_option("yacloud_group_label"))
 
         for instance in self.hosts:
             if group_label and group_label in instance["labels"]:
@@ -144,7 +156,9 @@ class InventoryModule(BaseInventoryPlugin, Constructable, Cacheable):
                 ip = self._get_ip_for_instance(instance)
                 if ip:
                     self.inventory.add_host(instance["name"], group=group)
-                    self.inventory.set_variable(instance["name"], 'ansible_host', to_native(ip))
+                    self.inventory.set_variable(
+                        instance["name"], "ansible_host", to_native(ip)
+                    )
 
     def parse(self, inventory, loader, path, cache=True):
         super(InventoryModule, self).parse(inventory, loader, path)

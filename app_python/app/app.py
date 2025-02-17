c7c627fb10fe6b99d078e0ef566c7844ac2a95e0
mihdenis85 <mihdenis777@yandex.ru>
2025-02-18 01:16:40 +0300

    Lab 8 Denis Mikhailov SD-03

--- app_python/app/app.py
+++ app_python/app/app.py
@@ -1,29 +1,27 @@
 from datetime import datetime
-from os import environ
 
 import pytz
+from prometheus_flask_exporter import PrometheusMetrics
+from os import environ
 from flask import Flask, render_template
 
 HOST = environ.get("HOST", "0.0.0.0")
 PORT = int(environ.get("PORT", "8080"))
 
-
-def create_app():
-    app = Flask(__name__)
-
-    @app.route("/", methods=["GET"])
-    def index():
-        """
-        Returns html page with current Moscow time.
-        """
-        moscow_timezone = pytz.timezone("Europe/Moscow")
-        time = datetime.now(moscow_timezone)
-        return render_template("index.html", time=time)
-
-    return app
+app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+metrics.info("app_info", "Application info", version="1.0.0")
 
 
-app = create_app()
+@app.route("/", methods=["GET"])
+def index():
+    """
+    Returns html page with current Moscow time.
+    """
+    moscow_timezone = pytz.timezone("Europe/Moscow")
+    time = datetime.now(moscow_timezone)
+    return render_template("index.html", time=time)
+
 
 if __name__ == "__main__":
-    app.run(host=HOST, port=PORT, debug=False)
+    app.run(host=HOST, port=PORT)
